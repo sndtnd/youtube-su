9410e0221d2af95c109d756fce4fb04eb0bc43a9
dnsdnt <tndsnd@gmail.com>
2013-10-04 07:16:03 -0700

    youtube.rc

--- youtube.sh
+++ youtube.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-email=""
-password=""
+mapfile -t creds < <(cat ~/.youtube-su.rc )
+email=$(echo "${creds[0]}")
+password=$(echo "${creds[1]}")
+#email=""
+#password=""
 cd /tmp/
 mkdir youtube-su
 cd youtube-su
 echo "" > youtube.list
-youtube-dl --max-quality 22 --prefer-free-formats --get-id -t -i -c -u "$email" -p "$password" http://www.youtube.com/feed/subscriptions >> youtube.list
+time youtube-dl --max-quality 22 --prefer-free-formats --get-id -t -i -c -u "$email" -p "$password" http://www.youtube.com/feed/subscriptions >> youtube.list
+sed -i -e "s/-/\\-/g;s/\&/\\&/g;s/\'/\\'/g"
